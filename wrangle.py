f5451fd44d11ae061c05b3c6d0aa941a9791f82d
Chris Joakim <christopher.joakim@gmail.com>
2018-02-27 15:47:38 -0500

    wrangle process - extract_top_ratings and extract_top_movies

--- wrangle.py
+++ wrangle.py
@@ -48,14 +48,9 @@ class Main:
         else:
             self.print_options('Error: no function argument provided.')
 
-    def print_options(self, msg):
-        print(msg)
-        arguments = docopt(__doc__, version=VERSION)
-        print(arguments)
-
     def extract_top_ratings(self):
         # Identify and extract the top (movie) ratings with at least 10000 votes
-        infile  = self.c.data_filename('title.ratings.tsv')
+        infile = self.c.data_filename('title.ratings.tsv')
         outfile = self.c.top_ratings_csv_filename()
         row_count = 0
         min_votes = 10000
@@ -92,27 +87,64 @@ class Main:
         # lines_read: 4832632  elapsed: 25.33212375640869
 
     def extract_top_movies(self):
-        infile  = self.c.data_filename('title.basics.tsv')
+        infile = self.c.data_filename('title.basics.tsv')
         outfile = self.c.top_movies_csv_filename()
+        selected = dict()
         row_count = 0
-        min_votes = 10000
-        selected  = dict()
+        top_rated = self.load_top_ratings()
 
         with open(infile) as tsvfile:
             reader = csv.DictReader(tsvfile, dialect='excel-tab')
             for row in reader:
-                # OrderedDict([('tconst', 'tt0000001'), ('averageRating', '5.8'), ('numVotes', '1349')])
+                # OrderedDict([('tconst', 'tt0000009'), ('titleType', 'movie'), ('primaryTitle', 'Miss Jerry'),
+                # ('originalTitle', 'Miss Jerry'), ('isAdult', '0'), ('startYear', '1894'), ('endYear', '\\N'),
+                # ('runtimeMinutes', '45'), ('genres', 'Romance')])
                 try:
                     row_count = row_count + 1
-                    votes = int(row['numVotes'])
-                    if votes > min_votes:
-                        id = row['tconst']
-                        selected[id] = votes
-                    # if row_count < 10:
-                    #     print(row)
-                    #     print(row['tconst'])
+                    id = row['tconst']
+                    if id in top_rated:
+                        if 'movie' == row['titleType']:
+                            if '0' == row['isAdult']:
+                                title = row['primaryTitle']
+                                selected[id] = title
+                                #print('selected top_rated item {} {}'.format(id, title))
                 except:
                     print('exception on row {} {}'.format(row_count, row))
+
+        print("extract_top_movies - selected count: {}".format(len(selected.keys())))
+
+        with open(outfile, "w", newline="\n") as out:
+            out.write("id|title\n")
+            for id in sorted(selected.keys()):
+                title = selected[id]
+                line  = '{}|{}'.format(id.strip(), title.strip())
+                out.write(line + "\n")
+            print('file written: {}'.format(outfile))
+
+    # private
+
+    def load_top_ratings(self):
+        infile1 = self.c.top_ratings_csv_filename()
+        top_rated = dict()
+        row_count = 0
+        with open(infile1) as csvfile:
+            reader = csv.reader(csvfile, delimiter='|')
+            for row in reader:
+                # ['tt0000417', '34795']
+                try:
+                    row_count = row_count + 1
+                    if row_count > 1:
+                        id = row[0]
+                        top_rated[id] = 0
+                except:
+                    print('exception on row {} {}'.format(row_count, row))
+        print('loaded the top_rated ids; count: {}'.format(len(top_rated.keys())))
+        return top_rated
+
+    def print_options(self, msg):
+        print(msg)
+        arguments = docopt(__doc__, version=VERSION)
+        print(arguments)
 
 
 Main().execute()
--- wrangle.sh
+++ wrangle.sh
@@ -3,12 +3,15 @@
 # Chris Joakim, Microsoft, 2018/02/27
 
 python wrangle.py extract_top_ratings
-
 head /Users/cjoakim/Downloads/imdb/top_ratings.csv
 wc   /Users/cjoakim/Downloads/imdb/top_ratings.csv
 
-# python wrangle.py extract_top_movies
+python wrangle.py extract_top_movies
+head /Users/cjoakim/Downloads/imdb/top_movies.csv
+wc   /Users/cjoakim/Downloads/imdb/top_movies.csv
 
-# wc /Users/cjoakim/Downloads/imdb/top_movies.csv
+cat /Users/cjoakim/Downloads/imdb/top_movies.csv | grep Footloose
+cat /Users/cjoakim/Downloads/imdb/top_movies.csv | grep Pretty
+cat /Users/cjoakim/Downloads/imdb/top_movies.csv | grep Blade
 
 echo 'done'
