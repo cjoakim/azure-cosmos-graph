93f3a29535b3f9496c611f821f33207d3370369e
Chris Joakim <christopher.joakim@gmail.com>
2018-03-11 18:26:41 -0400

    d3

--- webserver.sh
+++ webserver.sh
@@ -7,6 +7,8 @@
 #
 # Then visit http://localhost:8000/d3/index.html with your browser.
 #
+# See http://bl.ocks.org/fancellu/2c782394602a93921faff74e594d1bb1
+#
 # Chris Joakim, Microsoft, 2018/03/11
 
 python -m http.server 8000
